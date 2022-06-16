a734f8232f7ce4c01d08225b57dbbe3b13e2ef40
Owen Voke <owen.voke@worksome.com>
2022-06-16 16:34:26 +0100

    fix: resolve import of NodeList class

--- src/Inspections/IgnoreByNameSuppressorInspection.php
+++ src/Inspections/IgnoreByNameSuppressorInspection.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Worksome\Graphlint\Inspections;
 
 use GraphQL\Language\AST\Node;
+use GraphQL\Language\AST\NodeList;
 use Worksome\Graphlint\Contracts\SuppressorInspection;
 use Worksome\Graphlint\Utils\NodeNameResolver;
 
